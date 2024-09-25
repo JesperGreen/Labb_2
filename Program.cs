c8ae1f9d360baa15c146c34533a41645648e1591
JesperGreen <jesper_gs_@hotmail.com>
2024-09-26 00:33:28 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Labb_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store myStore = new Store();
+            myStore.Start();
+        }
+    }
+}
