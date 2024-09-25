8ea85fe18646e59b2d3587504516fae66cf26a6e
JesperGreen <jesper_gs_@hotmail.com>
2024-09-26 01:21:19 +0200

    Update Products.cs

--- Products.cs
+++ Products.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Labb_2
 {
-    internal class Product
+    public class Product
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
 
+        public Product(string name, decimal price) 
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        } 
     }
 }
