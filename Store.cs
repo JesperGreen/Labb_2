ae0f1c473eb190057631c35f57c5d3f68f20d3db
JesperGreen <jesper_gs_@hotmail.com>
2024-09-26 00:52:01 +0200

    Update Store.cs
    Fancy Pancy

--- Store.cs
+++ Store.cs
@@ -11,18 +11,29 @@ namespace Labb_2
     {
         public void Start() 
         {
-            /*           WriteLine("Log in");
+            Title = "Jesper's Essentials";
+            RunMainMenu();
+        }
 
-                       WriteLine("Register new account");
-            */
+        private void RunMainMenu()
+        {
+            string prompt = @"
+       _                           _         ______                    _   _       _     
+      | |                         ( )       |  ____|                  | | (_)     | |    
+      | | ___  ___ _ __   ___ _ __|/ ___    | |__   ___ ___  ___ _ __ | |_ _  __ _| |___ 
+  _   | |/ _ \/ __| '_ \ / _ \ '__| / __|   |  __| / __/ __|/ _ \ '_ \| __| |/ _` | / __|
+ | |__| |  __/\__ \ |_) |  __/ |    \__ \   | |____\__ \__ \  __/ | | | |_| | (_| | \__ \
+  \____/ \___||___/ .__/ \___|_|    |___/   |______|___/___/\___|_| |_|\__|_|\__,_|_|___/
+                  | |                                                                    
+                  |_|                                                                    
 
-            string prompt = "Welcome to Jesper's Essentials! What would you like to do? Please navigate your options using Up & Down arrows and select with Enter/Return.";
+
+Welcome to Jesper's Essentials! What would you like to do? 
+Please navigate your options using Up & Down arrows and select with Enter/Return.";
             string[] options = { "Log in", "Register new account", "Exit" };
             Menu mainMenu = new Menu(prompt, options);
             int selectedIndex = mainMenu.Run();
 
-
-            ReadKey(true);
         }
     }
 }
