43f7a611464d0ab998060205439e6d2bd24b04d3
JesperGreen <jesper_gs_@hotmail.com>
2024-09-26 04:22:50 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,25 +5,10 @@ namespace Labb_2
 {
     class Program
     {
-        static List<Customer> customers = new List<Customer>();
-        static List<Product> products = new List<Product>();
-
         static void Main(string[] args)
         {
             Store myStore = new Store();
             myStore.Start();
-
-            products.Add(new Product("test", 123));
-            products.Add(new Product("test", 123));
-            products.Add(new Product("test", 123));
-            products.Add(new Product("test", 123));
-            products.Add(new Product("test", 123));
-
-
-            customers.Add(new Customer("Knatte", "123"));
-            customers.Add(new Customer("Fnatte", "321"));
-            customers.Add(new Customer("Tjatte", "213"));
-
         }
     }
 }
