108ff9bbda0c9075a7d4a61b8f1d605980aad435
JesperGreen <jesper_gs_@hotmail.com>
2024-10-06 22:51:55 +0200

    Update CurrencyService.cs

--- CurrencyService.cs
+++ CurrencyService.cs
@@ -10,6 +10,21 @@ namespace Labb_2
 {
     public class CurrencyService
     {
+        private static readonly HttpClient client = new HttpClient();
 
+        private async Task<ExchangeRates> GetExchangeRatesAsync() 
+        {
+            string url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_4Nv0XzB6XS0cOLwfFEA5XFvjCxT3KPb9hJZBHVLw";
+            string json = await client.GetStringAsync(url);
+
+            var rates = JsonSerializer.Deserialize<ExchangeRates>(json);
+            return rates;
+        }
+    }
+
+    public class ExchangeRates
+    {
+        public decimal EUR {  get; set; }
+        public decimal USD { get; set; }
     }
 }
